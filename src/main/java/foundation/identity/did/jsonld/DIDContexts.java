d6b76125a50f6a682873d90420a0b84a99e1f5e9
Markus Sabadello <markus@danubetech.com>
2020-10-14 09:29:26 +0200

    fix: Fix loading of JSON-LD contexts.
    Signed-off-by: Markus Sabadello <markus@danubetech.com>

--- src/main/java/foundation/identity/did/jsonld/DIDContexts.java
+++ src/main/java/foundation/identity/did/jsonld/DIDContexts.java
@@ -29,15 +29,15 @@ public class DIDContexts {
             CONTEXTS = new HashMap<>();
 
             CONTEXTS.put(JSONLD_CONTEXT_W3_NS_DID_V1,
-                    JsonDocument.of(MediaType.JSON_LD, JsonLDObject.class.getResourceAsStream("diddocument-context-w3-ns-did-v1.jsonld")));
+                    JsonDocument.of(MediaType.JSON_LD, DIDContexts.class.getResourceAsStream("diddocument-context-w3-ns-did-v1.jsonld")));
             CONTEXTS.put(JSONLD_CONTEXT_W3_2019_DID_V1,
-                    JsonDocument.of(MediaType.JSON_LD, JsonLDObject.class.getResourceAsStream("diddocument-context-w3-did-v1.jsonld")));
+                    JsonDocument.of(MediaType.JSON_LD, DIDContexts.class.getResourceAsStream("diddocument-context-w3-2019-did-v1.jsonld")));
             CONTEXTS.put(JSONLD_CONTEXT_W3ID_DID_V1,
-                    JsonDocument.of(MediaType.JSON_LD, JsonLDObject.class.getResourceAsStream("diddocument-context-w3id-did-v1.jsonld")));
+                    JsonDocument.of(MediaType.JSON_LD, DIDContexts.class.getResourceAsStream("diddocument-context-w3id-did-v1.jsonld")));
             CONTEXTS.put(JSONLD_CONTEXT_W3ID_DID_V011,
-                    JsonDocument.of(MediaType.JSON_LD, JsonLDObject.class.getResourceAsStream("diddocument-context-w3id-did-v011.jsonld")));
+                    JsonDocument.of(MediaType.JSON_LD, DIDContexts.class.getResourceAsStream("diddocument-context-w3id-did-v011.jsonld")));
             CONTEXTS.put(JSONLD_CONTEXT_W3ID_VERESONE_V1,
-                    JsonDocument.of(MediaType.JSON_LD, JsonLDObject.class.getResourceAsStream("diddocument-context-w3id-veresone-v1.jsonld")));
+                    JsonDocument.of(MediaType.JSON_LD, DIDContexts.class.getResourceAsStream("diddocument-context-w3id-veresone-v1.jsonld")));
 
             for (Map.Entry<URI, JsonDocument> context : CONTEXTS.entrySet()) {
                 context.getValue().setDocumentUrl(context.getKey());
