4c2d48d6ad1d18c265ab85609d1cc24f8e050276
Markus Sabadello <markus@danubetech.com>
2021-06-15 12:38:50 +0200

    feat: Add constants for DID URL parameters.

---
+++ src/main/java/foundation/identity/did/parameters/Parameters.java
@@ -0,0 +1,10 @@
+package foundation.identity.did.parameters;
+
+public class Parameters {
+
+    public static final String DID_URL_PARAMETER_SERVICE = "service";
+    public static final String DID_URL_PARAMETER_RELATIVEREF = "relativeRef";
+    public static final String DID_URL_PARAMETER_VERSIONID = "versionId";
+    public static final String DID_URL_PARAMETER_VERSIONTIME = "versionTime";
+    public static final String DID_URL_PARAMETER_HL = "hl";
+}
