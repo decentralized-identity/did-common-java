08afbc4777b16a7b2610690e9eac4f09cae4b683
Markus Sabadello <markus@danubetech.com>
2021-06-21 18:35:19 +0200

    test: Additional test for DID documents.

---
+++ src/test/java/foundation/identity/did/DIDDocumentTest.java
@@ -0,0 +1,67 @@
+package foundation.identity.did;
+
+import foundation.identity.did.parser.ParserException;
+import foundation.identity.jsonld.JsonLDDereferencer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DIDDocumentTest {
+
+	@Test
+	public void testDIDDocument() throws Exception {
+
+		// DID DOCUMENT verificationMethods
+
+		List<VerificationMethod> verificationMethods = new ArrayList<>();
+
+		VerificationMethod verificationMethod = VerificationMethod.builder()
+				.id(URI.create("did:ex:123#key-1"))
+				.types(Arrays.asList("Ed25519VerificationKey2018"))
+				.publicKeyBase58("000000000")
+				.build();
+
+		verificationMethods.add(verificationMethod);
+
+		// DID DOCUMENT services
+
+		List<Service> services = new ArrayList<Service>();
+
+		Service service = Service.builder()
+				.type("DIDCommService")
+				.serviceEndpoint("http://localhost:8080/")
+				.build();
+
+		services.add(service);
+
+		// create DID DOCUMENT
+
+		DIDDocument didDocument = DIDDocument.builder()
+				.id(URI.create("did:ex:123"))
+				.verificationMethods(verificationMethods)
+				.authenticationVerificationMethods(Collections.singletonList(VerificationMethod.builder().id(verificationMethod.getId()).build()))
+				.assertionMethodVerificationMethods(Collections.singletonList(VerificationMethod.builder().id(verificationMethod.getId()).build()))
+				.services(services)
+				.build();
+
+		// tests
+
+		assertEquals(URI.create("did:ex:123"), didDocument.getId());
+		assertEquals("Ed25519VerificationKey2018", didDocument.getVerificationMethods().get(0).getType());
+		assertEquals(URI.create("did:ex:123#key-1"), didDocument.getAuthenticationVerificationMethods().get(0).getId());
+		assertEquals(URI.create("did:ex:123#key-1"), didDocument.getAssertionMethodVerificationMethods().get(0).getId());
+		assertEquals("did:ex:123#key-1", ((List<String>) didDocument.getJsonObject().get("authentication")).get(0));
+		assertEquals("did:ex:123#key-1", ((List<String>) didDocument.getJsonObject().get("assertionMethod")).get(0));
+		assertEquals(URI.create("did:ex:123#key-1"), didDocument.getAssertionMethodVerificationMethods().get(0).getId());
+		assertEquals("DIDCommService", didDocument.getServices().get(0).getType());
+		assertEquals("http://localhost:8080/", didDocument.getServices().get(0).getServiceEndpoint());
+		assertEquals("http://localhost:8080/", ((List<Map<String, Object>>) didDocument.getJsonObject().get("service")).get(0).get("serviceEndpoint"));
+
+		assertEquals("000000000", VerificationMethod.fromJsonObject(JsonLDDereferencer.findByIdInJsonLdObject(didDocument, didDocument.getAuthenticationVerificationMethods().get(0).getId(), null).getJsonObject()).getPublicKeyBase58());
+		assertEquals("000000000", VerificationMethod.fromJsonObject(JsonLDDereferencer.findByIdInJsonLdObject(didDocument, didDocument.getAssertionMethodVerificationMethods().get(0).getId(), null).getJsonObject()).getPublicKeyBase58());
+	}
+}
